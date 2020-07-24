23355d694bd8448b122daca1396c9be35893bf5b
thehanimo <hani.mohammed.3@gmail.com>
2020-07-24 14:09:32 +0530

    Combine reccs

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var pool = require("../queries");
 const passport = require("passport");
 
-const recommendations = require("../utils/recommendation"); 
+const recommendations = require("../utils/recommendation");
 
 async function convertFavourites(table, arr) {
   if (arr.length == 0) return [];
@@ -16,40 +16,40 @@ async function convertFavourites(table, arr) {
     `SELECT * from ${table} where id IN (${params.join(",")})`,
     arr
   );
-  return results.rows;
+  return results.rows.map((x) => ({ ...x, type: table }));
 }
 
 function createRecommendationRoute(tableName, recommendationFunction) {
-    return function(req, res) {
-        const recommended = recommendationFunction(req.user.id).then((recs => {
-          if (recs.len == 0) {
-            res.json([]);
-            return;
-          }
-          convertFavourites(tableName, recs).then(converted => {
-            console.log(converted);
-            res.json(converted);
-          });
-        }));
-    };
+  return function (req, res) {
+    const recommended = recommendationFunction(req.user.id).then((recs) => {
+      if (recs.len == 0) {
+        res.json([]);
+        return;
+      }
+      convertFavourites(tableName, recs).then((converted) => {
+        console.log(converted);
+        res.json(converted);
+      });
+    });
+  };
 }
 
 router.get(
-    "/course",
-    passport.authenticate("jwt", { session: false }),
-    createRecommendationRoute("course", recommendations.recommendCourse)
-  );
+  "/course",
+  passport.authenticate("jwt", { session: false }),
+  createRecommendationRoute("course", recommendations.recommendCourse)
+);
 
 router.get(
-    "/book",
-    passport.authenticate("jwt", { session: false }),
-    createRecommendationRoute("book", recommendations.recommendBooks)
-  );
+  "/book",
+  passport.authenticate("jwt", { session: false }),
+  createRecommendationRoute("book", recommendations.recommendBooks)
+);
 
 router.get(
-    "/playlist",
-    passport.authenticate("jwt", { session: false }),
-    createRecommendationRoute("playlist", recommendations.recommendPlaylist)
-  );
+  "/playlist",
+  passport.authenticate("jwt", { session: false }),
+  createRecommendationRoute("playlist", recommendations.recommendPlaylist)
+);
 
 module.exports = router;
